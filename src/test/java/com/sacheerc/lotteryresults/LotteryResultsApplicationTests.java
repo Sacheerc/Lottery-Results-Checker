e58b313fdab2da03706e4aa608a14ca8b7dd83c0
sacheerc <madushansachintha@gmail.com>
2023-09-12 22:10:30 +0530

    ENH:2:Initialize the spring application

---
+++ src/test/java/com/sacheerc/lotteryresults/LotteryResultsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sacheerc.lotteryresults;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LotteryResultsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
